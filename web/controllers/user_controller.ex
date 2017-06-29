2a238d6f5b72aa3282f85fcd77604033018888a5
Kenneth Bogner <bognerkenny@gmail.com>
2017-06-28 21:23:58 -0400

    Added changes

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate_user when action in [:index, :show]
 
   alias Rumbl.User
 
@@ -19,11 +20,11 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    IO.inspect(user_params)
     changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
---
+++ web/models/video.ex
@@ -0,0 +1,47 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    field :slug, :string
+    belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:url, :title, :description])
+    |> slugify_title()
+    |> validate_required([:url, :title, :description])
+    |> assoc_constraint(:category)
+  end
+
+  def slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  def slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
+  end
+end
